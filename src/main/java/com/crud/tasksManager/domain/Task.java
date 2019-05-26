03590a1b02c5bbb0e2ae6796c77dc2b69557ca65
Anna Leja <anna.leja16@gmail.com>
2019-05-26 18:42:39 +0200

    add entities

--- src/main/java/com/crud/tasksManager/domain/Task.java
+++ src/main/java/com/crud/tasksManager/domain/Task.java
@@ -4,14 +4,21 @@ package com.crud.tasksManager.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Entity
+@Entity(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
