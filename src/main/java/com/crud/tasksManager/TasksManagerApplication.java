7d0097f5df7bb1e0ed0e3e759ed92b706110b72f
Anna Leja <anna.leja16@gmail.com>
2019-06-02 21:35:02 +0200

    add SpringApplicationBuilder

--- src/main/java/com/crud/tasksManager/TasksManagerApplication.java
+++ src/main/java/com/crud/tasksManager/TasksManagerApplication.java
@@ -3,13 +3,20 @@ package com.crud.tasksManager;
 import com.crud.tasksManager.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksManagerApplication {
+public class TasksManagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(TasksManagerApplication.class, args);
 	}
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TasksManagerApplication.class);
+	}
 }
