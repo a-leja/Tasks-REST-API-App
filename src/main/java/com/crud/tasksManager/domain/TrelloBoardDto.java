af7138d660884f554b4e379736ab846a2fc98143
Anna Leja <anna.leja16@gmail.com>
2019-06-28 18:06:40 +0200

    add api client

---
+++ src/main/java/com/crud/tasksManager/domain/TrelloBoardDto.java
@@ -0,0 +1,14 @@
+package com.crud.tasksManager.domain;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrelloBoardDto {
+
+       private String name;
+       private String id;
+}
