ff5c97ecab182e66ddce752f0fead1e6a11f7eea
Anna Leja <anna.leja16@gmail.com>
2019-07-10 21:32:34 +0200

    add annotations

--- src/main/java/com/crud/tasksManager/domain/AttachmentsByType.java
+++ src/main/java/com/crud/tasksManager/domain/AttachmentsByType.java
@@ -1,6 +1,16 @@
 package com.crud.tasksManager.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AttachmentsByType {
 
+    @JsonProperty("trello")
     private Trello trello;
 }
