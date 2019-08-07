f3ed4d5b334b4fc527c57cb6b9fee111b325a029
Anna Leja <anna.leja16@gmail.com>
2019-08-07 11:02:57 +0200

    add CC condition

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -32,6 +32,10 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
+        if (mail.getToCc() != null) {
+            mailMessage.setCc(mail.getToCc());
+        }
         return mailMessage;
     }
+
 }
