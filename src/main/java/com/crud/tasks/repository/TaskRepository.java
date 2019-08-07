79db4980d12ef370329211c7a3359e9b914d15ef
Anna Leja <anna.leja16@gmail.com>
2019-08-07 21:51:23 +0200

    add scheduler

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -2,7 +2,6 @@ package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -19,4 +18,7 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
 
     @Override
     void deleteById(Long id);
+
+    @Override
+    long count();
 }
