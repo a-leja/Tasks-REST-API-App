fa1d84cc2af130c4563ef0703eb6913ec87a4dae
Anna Leja <anna.leja16@gmail.com>
2019-08-21 22:30:28 +0200

    refactor getForObject method

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -91,7 +91,7 @@ public class TrelloClientTest {
 
         URI uri = new URI("http://test.com/members/alcourse/boards?key=test&token=test&fields=name,id&lists=all");
 
-        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
+        when(restTemplate.getForObject(uri, TrelloBoardDto.class)).thenReturn(null);
 
         //When
         List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
