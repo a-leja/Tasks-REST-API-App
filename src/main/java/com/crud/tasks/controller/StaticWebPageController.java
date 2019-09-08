a93c91e251bb67541ee1365c159016f748827242
Anna <anna.leja16@gmail.com>
2019-09-08 16:06:56 +0200

    update Thymeleaf

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -9,8 +9,7 @@ public class StaticWebPageController {
 
     @RequestMapping("/")
     public String index(Map<String, Object> model) {
-        model.put("variable", "My Thymeleaf variable");
-        model.put("two", 2);
+        model.put("title", "TasksManager");
         return "index";
     }
 }
