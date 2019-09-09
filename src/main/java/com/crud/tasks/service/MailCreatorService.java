a546ff813fc3a59de57b1ccb3248435ed17e82a6
Anna <anna.leja16@gmail.com>
2019-09-09 21:14:38 +0200

    add daily-update-email

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -36,14 +36,36 @@ public class MailCreatorService {
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("show_button", false);
+        context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("company_name", companyDetails.getCompanyName());
         context.setVariable("company_goal", companyDetails.getCompanyGoal());
         context.setVariable("company_contact_details", companyDetails.getCompanyEmail() + companyDetails.getCompanyPhone());
         context.setVariable("is_friend", false);
         context.setVariable("admin_config", adminConfig);
         context.setVariable("application_functionality", functionality);
-        return templateEngine.process("mail/created-trello-card-mail",context);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildDailyUpdateEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add(("You can manage your tasks"));
+        functionality.add(("Provides connection with Trello Account"));
+        functionality.add(("Application allows sending tasks to Trello"));
+
+        Context context = new Context();
+        context.setVariable("preview", "Trello app - Your daily update is here!");
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Check your tasks");
+                context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", companyDetails.getCompanyName());
+        context.setVariable("company_goal", companyDetails.getCompanyGoal());
+        context.setVariable("company_contact_details", companyDetails.getCompanyEmail() + companyDetails.getCompanyPhone());
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/daily-update-email", context);
     }
 }
